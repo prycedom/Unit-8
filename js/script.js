3fa39ba62ac5629e95d2b7b5001eb8764b9b6e8e
prycedom <prycedom@gmail.com>
2020-08-02 21:32:46 -0400

    Added next and back btns

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@ const grid = document.querySelector(".grid");
 const overlay = document.querySelector(".overlay");
 const modalContainer = document.querySelector(".modal-content");
 const modalClose = document.querySelector(".modal-close");
-
+const nextEmployeeBtn = document.querySelector("#nextEmployeeBtn");
+const prevEmployeeBtn = document.querySelector("#prevEmployeeBtn");
 
 fetch(urlAPI)
     .then(response => {
@@ -50,32 +51,68 @@ function displayPeople(people) {
 
 function displayModal(index) {
     // use object destructuring make our template literal cleaner
-    let { name, dob, phone, email, location: { city, street, state, postcode
-    }, picture } = employees[index];
+    let {
+        name,
+        dob,
+        phone,
+        email,
+        location: {
+            city,
+            street,
+            state,
+            postcode
+        },
+        picture
+    } = employees[index];
+
     let date = new Date(dob.date);
     const modalHTML = `
     <img class="photo" src="${picture.large}" />
     <div class="text-container">
-    <h2 class="name">${name.first} ${name.last}</h2>
-    <p class="email">${email}</p>
-    <p class="address">${city}</p>
-    <hr />
-    <p>${phone}</p>
-    <p class="address">${street.number} ${street.name}, ${state} ${postcode}</p>
-    <p>Birthday:
-    ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
+        <h2 class="name">${name.first} ${name.last}</h2>
+        <p class="email">${email}</p>
+        <p class="address">${city}</p>
+        <hr />
+        <p>${phone}</p>
+        <p class="address">${street.number} ${street.name}, ${state} ${postcode}</p>
+        <p>Birthday:
+        ${date.getMonth()}/${date.getDate()}/${date.getFullYear()}</p>
     </div>
+    <button id="employeeBackBtn">Back</button>
+    <button id="employeeNextBtn">Next</button>
     `;
+
     overlay.classList.remove("hidden");
     modalContainer.innerHTML = modalHTML;
-    }
+
+    const nextEmployeeBtn = document.querySelector("#employeeNextBtn");
+    const prevEmployeeBtn = document.querySelector("#employeeBackBtn");
+
+    nextEmployeeBtn.addEventListener('click', () => {
+        if(index == employees.length-1) {
+            displayModal(0);
+        }
+        else {
+            displayModal(index+1);
+        }
+    });
+
+    prevEmployeeBtn.addEventListener('click', () => {
+        if(index == 0) {
+            displayModal(employees.length-1);
+        }
+        else {
+            displayModal(index-1);
+        }
+    });
+}
     
     grid.addEventListener('click', e => {
         // make sure the click is not on the gridContainer itself
         if (e.target !== grid) {
         // select the card element based on its proximity to actual element
         const card = e.target.closest(".person");
-        const index = card.getAttribute('data-index');
+        const index =parseInt(card.getAttribute('data-index'));
         displayModal(index);
         }
         });
